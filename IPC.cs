62d6103b98bf9ac90f175f39df5b240dafa9c413
Urooj Pawan <pari.advani4@gmail.com>
2021-05-04 12:54:39 +0500

    Add files via upload (#2)

---
+++ IPC.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace ConsumerPortalNetService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IPC" in both code and config file together.
+    [ServiceContract]
+    public interface IPC
+    {
+        //[OperationContract]
+        //[WebInvoke(Method = "GET", ResponseFormat = WebMessageFormat.Json)]
+        //Stream GetFeederData(string DTSID);
+
+
+        //[OperationContract]
+        //[WebInvoke(Method = "GET", ResponseFormat = WebMessageFormat.Json)]
+        //Stream GetDuplicateBills(string ContractNo, int Month = 6);
+
+
+        [OperationContract]
+        [WebInvoke(Method = "GET", ResponseFormat = WebMessageFormat.Json)]
+        Stream GetBillPDF(string ContractNo, string BillingMonth);
+    }
+}
